979e74047e99262ce58f5eea2be74288d0d0883e
James Farris <jafarris.exe@gmail.com>
2023-03-22 18:27:53 -0400

    Added prompt and confirmation windows

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,26 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
+// Create function to generate the password
+// generatePassword has to be assigned to something
+
+function generatePassword() {
+  // Write a prompt for criteria
+  // Prompt for length 8-128 characters
+  let length = prompt(
+    "How many characters would you like your password to be?"
+  );
+  // Character type confirmations
+  let lowerCase = confirm("Would you like lowercase letters?");
+  let upperCase = confirm("Would you like uppercase letters?");
+  let numbers = confirm("Would you like numbers?");
+  let specialChar = confirm("Would you like special characters?");
+  // Input validation
+}
 // Write password to the #password input
 function writePassword() {
-  // Create function to generate the password
-
-  // generatePassword has to be assigned to something
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   // Replaces the "Your Secure Password with new password"
   passwordText.value = password;
