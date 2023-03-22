2d6bff12bfcb1b3f48e3b69d095418a8f503e631
James Farris <jafarris.exe@gmail.com>
2023-03-22 18:10:46 -0400

    Added pseudo code to break down problem

--- Develop/script.js
+++ Develop/script.js
@@ -3,11 +3,14 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  // Create function to generate the password
+
+  // generatePassword has to be assigned to something
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  // Replaces the "Your Secure Password with new password"
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
