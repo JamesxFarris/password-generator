a7d167414e3bf692802b58ed845dd5e678684c28
James Farris <jafarris.exe@gmail.com>
2023-03-23 05:07:51 -0400

    Added prompts to close if user enters incorrect field

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,7 @@
 // Assignment Code
-
-// Create empty password array
-let userChoice = [];
-// Create the minimum of 8 characters
-let charLength = 8;
-
+const generateBtn = document.querySelector("#generate");
 // Specify letters for lowercase
-let lowerCaseChars = [
+const lowerCaseChars = [
   "a",
   "b",
   "c",
@@ -36,7 +31,7 @@ let lowerCaseChars = [
 ];
 
 // Specify letters for uppercase
-let upperCaseChars = [
+const upperCaseChars = [
   "A",
   "B",
   "C",
@@ -66,10 +61,10 @@ let upperCaseChars = [
 ];
 
 // Specify numbers
-let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Specify special characters
-let specialChars = [
+const specialChars = [
   "!",
   '"',
   "#",
@@ -102,16 +97,18 @@ let specialChars = [
   "}",
   "~",
 ];
-
-let generateBtn = document.querySelector("#generate");
+// Create empty password array
+let userChoice = [];
+// Create the minimum of 8 characters
+let charLength = 8;
 
 // Create Function to get user inputs
 function grabInfo() {
   charLength = prompt(
     "How many characters would you like your password to be?"
   );
-  // Check number
-  if (charLength < 8 || charLength > 128) {
+  // Check number, return booleans to callback later if they get it wrong
+  if (charLength < 8 || charLength > 128 || isNaN(charLength)) {
     alert("Sorry, the number has to be between 8 - 128");
     return false;
   }
@@ -131,17 +128,20 @@ function grabInfo() {
   if (confirm("Would you like special characters?")) {
     userChoice = userChoice + specialChars;
   }
+  return true;
 }
-
-grabInfo();
 
 // Create function to generate the password
 function generatePassword() {}
 
-// Write password to the #password input
+// Write password to the #password input if the user entered fields properly
 function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
+  let pass = grabInfo();
+
+  if (pass) {
+    let password = generatePassword();
+    let passwordText = document.querySelector("#password");
+  }
 
   // Replaces the "Your Secure Password with new password"
   passwordText.value = password;
